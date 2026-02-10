f06367347f5ec0da93936a720aa3006c5b348d99
Matias Concha <167500389+matiasConcha1@users.noreply.github.com>
2026-02-10 17:16:30 -0300

    chore(config): actualizar settings y urls del proyecto

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,9 @@ STATICFILES_DIRS = (
 #if not DEBUG:
 #    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
